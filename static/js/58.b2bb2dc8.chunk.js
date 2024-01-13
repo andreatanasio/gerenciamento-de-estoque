d513d3f35c82cc92f29393e2d830e24e99e4f5e8
André Atanasio <andre.atanasio@gmail.com>
2024-01-13 19:58:13 -0300

    Updates

--- static/js/58.b2bb2dc8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgerenciamento_de_estoque=self.webpackChunkgerenciamento_de_estoque||[]).push([[58],{58:(e,s,t)=>{t.r(s),t.d(s,{default:()=>n});const n={}}}]);
-//# sourceMappingURL=58.b2bb2dc8.chunk.js.map
